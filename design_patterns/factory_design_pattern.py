31ab4832d5f68301f00189daae1b8316c97efbf3
AmitM0287 <amitmanna0287@gmail.com>
2021-03-31 15:12:45 +0530

    [Amit] Added: implementation of factory design pattern

--- design_patterns/factory_design_pattern.py
+++ design_patterns/factory_design_pattern.py
@@ -5,31 +5,43 @@
     in the future you might need a class C. I fwe use inheritance so, there many things we have to change.
     So, there is a better way to do this to use a design pattern to gets the objects.
 """
+from abc import ABC, abstractmethod
 
 
-class A(object):
+class Implementor(ABC):
+    @abstractmethod
+    def who_am_i(self):
+        pass
+
+
+class Computer(Implementor):
     def __init__(self):
         pass
 
-    def method_A(self):
-        print('I am method A')
+    def who_am_i(self):
+        print('I am Computer.')
 
 
-class B(object):
+class Television(Implementor):
     def __init__(self):
         pass
 
-    def method_B(self):
-        print('I am method B')
+    def who_am_i(self):
+        print('I am Television')
 
 
-def get(obj=''):
-    objs = dict(a=A(), b=B())
-    return objs[obj]
+class Factory:
+    def __init__(self):
+        pass
+
+    def get_instance_of(self, obj=''):
+        instance = dict(Computer=Computer(), Television=Television())
+        return instance[obj]
 
 
 if __name__ == '__main__':
-    a = get('a')
-    a.method_A()
-    b = get('b')
-    b.method_B()
+    factory = Factory()
+    computer = factory.get_instance_of('Computer')
+    computer.who_am_i()
+    television = factory.get_instance_of('Television')
+    television.who_am_i()
